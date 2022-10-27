6913d6594e5643770020325243d83e5fe08a12e6
greenwhale00 <goreakay@gmail.com>
2022-10-27 16:00:58 +0900

    221027

---
+++ doc/m011.php
@@ -0,0 +1,21 @@
+<?php
+include_once('../../../common.php');
+include_once(G5_THEME_PATH.'/head.php');
+?>
+
+
+<main>
+    <section class="main_visual">
+        <div class="main_slide">
+            <div>도</div>
+            <div>레</div>
+            <div>미</div>
+        </div>
+    </section>
+</main>
+
+
+
+
+<?php
+include_once(G5_THEME_PATH.'/tail.php');
